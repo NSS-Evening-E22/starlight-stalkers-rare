6b6c24730950661783c8098f9f471eef518fd2d5
Robert <robert.mabry86@gmail.com>
2023-08-07 20:08:48 -0500

    added the Categories APIs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,5 +20,26 @@ app.UseHttpsRedirection();
 
 //Create Endpoints Here!!!
 
-app.Run();
+//create category
+app.MapPost("/Categories/{Id}", (Categories Category) =>
+{
+    Category.Id = Category.Max(cat => cat.Id) + 1;
+    Category.Add(Category)
+    return Results.Ok(Categories);
+});
 
+//view all categories
+app.MapGet("/Categories", () =>
+{
+    return Categories;
+});
+
+//filter all posts by category id
+app.MapGet("/Posts/Categories/{Id}", (int Id) =>
+{
+    var filteredPosts = Posts.Where(post => categoryIds.Contains(post.CategoryId));
+    return Results.Ok(filteredPosts);
+
+});
+
+app.Run();
