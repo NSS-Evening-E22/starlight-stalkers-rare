9e530a5d585789d6ff828879cbca3eb07f945a3d
Jasper Baltz <nickolas.baltz@gmail.com>
2023-08-07 20:03:21 -0500

    fixed and refactored all dummy data so that it is linked across classes and will be able to be called

---
+++ dummyData/commentsData.cs
@@ -0,0 +1,75 @@
+using starlight_stalkers_rare.Models;
+
+List<Comment> comments = new List<Comment>
+{
+  new Comment
+  {
+    Id = 1,
+    AuthorId = 101,
+    PostId = 201,
+    Content = "Great post! Really enjoyed reading it."
+  },
+  new Comment
+  {
+    Id = 2,
+    AuthorId = 102,
+    PostId = 202,
+    Content = "Thanks for sharing this valuable information."
+  },
+  new Comment
+  {
+    Id = 3,
+    AuthorId = 103,
+    PostId = 203,
+    Content = "I have a question related to the post. Can you help?"
+  },
+  new Comment
+  {
+    Id = 4,
+    AuthorId = 102,
+    PostId = 204,
+    Content = "This post is inspiring. Keep up the great work!"
+  },
+  new Comment
+  {
+    Id = 5,
+    AuthorId = 105,
+    PostId = 205,
+    Content = "I learned something new from this article. Thanks!"
+  },
+  new Comment
+  {
+    Id = 6,
+    AuthorId = 104,
+    PostId = 206,
+    Content = "Your writing style is impressive. Enjoyed every bit of it."
+  },
+  new Comment
+  {
+    Id = 7,
+    AuthorId = 103,
+    PostId = 207,
+    Content = "I have bookmarked this page for future reference. Great content!"
+  },
+  new Comment
+  {
+    Id = 8,
+    AuthorId = 101,
+    PostId = 201,
+    Content = "I completely agree with the points mentioned in the post."
+  },
+  new Comment
+  {
+    Id = 9,
+    AuthorId = 102,
+    PostId = 203,
+    Content = "This is exactly what I was looking for. Thank you!"
+  },
+  new Comment
+  {
+    Id = 10,
+    AuthorId = 104,
+    PostId = 207,
+    Content = "Can't wait to read more articles from you. Well done!"
+  }
+};
---
+++ dummyData/usersData.cs
@@ -0,0 +1,65 @@
+using starlight_stalkers_rare.Models;
+
+List<User> users = new List<User>
+{
+  new User
+  {
+    Id = 101,
+    FirstName = "John",
+    LastName = "Doe",
+    Email = "john.doe@example.com",
+    Bio = "Software Engineer and aspiring artist",
+    Username = "johndoe87",
+    Password = "p@ssw0rd123",
+    CreatedOn = "2023-08-05T12:34:56Z",
+    Active = true
+  },
+  new User
+  {
+    Id = 102,
+    FirstName = "Jane",
+    LastName = "Smith",
+    Email = "jane.smith@example.com",
+    Bio = "Marketing specialist with a passion for travel",
+    Username = "janesmith",
+    Password = "M@rket1ng!",
+    CreatedOn = "2023-08-05T12:34:56Z",
+    Active = true
+  },
+  new User
+  {
+    Id = 103,
+    FirstName = "Michael",
+    LastName = "Johnson",
+    Email = "michael.johnson@example.com",
+    Bio = "Fitness enthusiast and personal trainer",
+    Username = "mikefit",
+    Password = "Strong@Pass123",
+    CreatedOn = "2023-08-05T12:34:56Z",
+    Active = true
+  },
+  new User
+  {
+    Id = 104,
+    FirstName = "Emily",
+    LastName = "Brown",
+    Email = "emily.brown@example.com",
+    Bio = "Book lover and aspiring writer",
+    Username = "emwrites",
+    Password = "BookWorm99!",
+    CreatedOn = "2023-08-05T12:34:56Z",
+    Active = true
+  },
+  new User
+  {
+    Id = 105,
+    FirstName = "David",
+    LastName = "Lee",
+    Email = "david.lee@example.com",
+    Bio = "Tech geek and gamer",
+    Username = "davidgamer",
+    Password = "G@mingF0rever",
+    CreatedOn = "2023-08-05T12:34:56Z",
+    Active = true
+  }
+};
