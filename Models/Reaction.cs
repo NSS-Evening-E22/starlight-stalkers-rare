56058f518372666140b3705d910a29f856463f68
Steve Butler <butlersteve.c@gmail.com>
2023-08-08 21:32:26 -0500

    add Subscription, Reaction, PostReaction models

---
+++ Models/Reaction.cs
@@ -0,0 +1,8 @@
+﻿namespace starlight_stalkers_rare.Models;
+
+public class Reaction
+{
+    public int Id { get; set; }
+    public int Emoji { get; set; }
+
+}
---
+++ Models/Subscription.cs
@@ -0,0 +1,9 @@
+﻿namespace starlight_stalkers_rare.Models;
+
+public class Subscription
+{
+    public int Id { get; set; }
+    public int AuthorId { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+}
